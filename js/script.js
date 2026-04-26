04a38a2ac1800b4358c043dc0ed14a0bef4ecc3c
aspasduplas <66entreaspasduplas@gmail.com>
2026-04-26 20:11:56 -0300

    so falta o tecnico agora zzz

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// Carrossel de Certificados
+let certSlideIndex = 1;
+showCertSlides(certSlideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusCertSlides(n) {
+  showCertSlides(certSlideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showCertSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let slides = document.getElementsByClassName("cert-slides");
+  if (n > slides.length) {certSlideIndex = 1}
+  if (n < 1) {certSlideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[certSlideIndex-1].style.display = "block";
+}
+
+// Carrossel de Fotos
+let fotoSlideIndex = 1;
+showFotoSlides(fotoSlideIndex);
+
+function plusFotoSlides(n) {
+  showFotoSlides(fotoSlideIndex += n);
+}
+
+function showFotoSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("foto-slides");
+  if (n > slides.length) {fotoSlideIndex = 1}
+  if (n < 1) {fotoSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[fotoSlideIndex-1].style.display = "block";
 }
